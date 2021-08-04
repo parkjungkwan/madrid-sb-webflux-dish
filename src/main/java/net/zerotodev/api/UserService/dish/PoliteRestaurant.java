cd2672b611d2cba2f607cfe976d78cdf552d317e
parkjungkwan <pakjkwan@gmail.com>
2021-08-04 17:55:13 +0900

    17

--- src/main/java/net/zerotodev/api/UserService/dish/PoliteRestaurant.java
+++ src/main/java/net/zerotodev/api/UserService/dish/PoliteRestaurant.java
@@ -1,5 +1,7 @@
 package net.zerotodev.api.UserService.dish;
-
+/**
+ * 구독(subscription) 을 하기 전에는 어떤 연산도 일어나 지 않는다.
+ * */
 public class PoliteRestaurant {
     public static void main(String... args) {
         PoliteServer server = new PoliteServer(new KitchenService());
