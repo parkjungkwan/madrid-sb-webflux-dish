78d0289caf72c0e8b80198deb460d4894d0b3cd3
parkjungkwan <pakjkwan@gmail.com>
2021-08-06 16:23:35 +0900

    16

--- src/main/java/net/zerotodev/api/UserService/person/SimpleBox.java
+++
@@ -1,4 +0,0 @@
-package net.zerotodev.api.UserService.person;
-
-public class SimpleBox {
-}
---
+++ src/main/java/net/zerotodev/api/UserService/util/StringDemo.java
@@ -0,0 +1,22 @@
+package net.zerotodev.api.UserService.util;
+
+public class StringDemo {
+    public String concat(String s1, String s2){
+        return s1.concat(s2);
+    }
+    public String substring(String s, int idx){
+        return s.substring(idx);
+    }
+    public String substring(String s, int start, int end){
+        return s.substring(start, end);
+    }
+    public int compareTo(String s1, String s2){
+        return s1.compareTo(s2);
+    }
+    public int compareToIgnoreCase(String s1, String s2){
+        return s1.compareToIgnoreCase(s2);
+    }
+    public String removeHyponeInJuminBunho(String juminBunho){
+        return ""; // p.263 로직 작성
+    }
+}
