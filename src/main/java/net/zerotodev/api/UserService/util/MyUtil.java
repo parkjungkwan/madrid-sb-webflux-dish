5587baca5dba624f7d5cba1e61044eab4416a52a
parkjungkwan <pakjkwan@gmail.com>
2021-08-06 14:24:05 +0900

    14

--- src/main/java/net/zerotodev/api/UserService/util/MyUtil.java
+++ src/main/java/net/zerotodev/api/UserService/util/MyUtil.java
@@ -14,10 +14,16 @@ public class MyUtil {
     }
     public String usedTime(String hour1, String min1, String sec1,
                            String hour2, String min2, String sec2){
-        return "총 이용시간: "+Duration.between(
-                getLocalTime(hour1, min1, sec1),getLocalTime(hour2, min2, sec2));
+        return "총 이용시간: "+getTime((int)Duration.between(
+                getLocalTime(hour1, min1, sec1),getLocalTime(hour2, min2, sec2)).getSeconds());
     }
     private LocalTime getLocalTime(String hour, String min, String sec){
         return LocalTime.of(Integer.parseInt(hour),Integer.parseInt(min),Integer.parseInt(sec));
+    }
+    private String getTime(int sec){
+        int hour = sec / (60*60);
+        int minute =(sec - hour*(60*60))/60;
+        int second = sec % 60;
+        return String.format("%d 시간, %d 분, %s 초", hour, minute, second);
     }
 }
